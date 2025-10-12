f27e2a9224dad385dfb216f8c384bb647e371dbb
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-12 18:03:58 +0300

    average

--- AverageOfThreeNumber.py
+++ AverageOfThreeNumber.py
@@ -14,5 +14,6 @@ print(d)
 x = float(input("Enter first number: "))
 y = float(input("Enter second number: "))
 z = float(input("Enter third number: "))
+t = float(input("Enter the devidation number: "))
 
-print(f'The average of {x}, {y}, and {z} is: {(x + y + z) / 3:.2f}')
+print(f'The average of {x}, {y}, and {z} is: {(x + y + z) / t:.2f}')
