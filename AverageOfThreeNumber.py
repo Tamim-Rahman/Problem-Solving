73886383a7d9388441c7f810af05bf8b84846f1d
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-12 18:01:30 +0300

    Average

---
+++ AverageOfThreeNumber.py
@@ -0,0 +1,18 @@
+#Calculate the average of 3 numbers
+
+#Solution
+
+a=2
+b=3
+c=4
+d= (a+b+c)/3
+print(d)
+
+
+#Input from user
+
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+z = float(input("Enter third number: "))
+
+print(f'The average of {x}, {y}, and {z} is: {(x + y + z) / 3:.2f}')
