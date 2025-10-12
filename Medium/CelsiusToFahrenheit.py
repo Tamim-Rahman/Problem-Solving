766f6d03814250503c3779ec71486a892e218089
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-12 17:42:02 +0300

    Remainder

---
+++ Medium/CelsiusToFahrenheit.py
@@ -0,0 +1,9 @@
+#Convert Celsius to Fahrenheit (Formula: F = (C × 9/5) + 32)
+
+
+#Solution
+
+celsius = float(input("Enter the current celsius: "))
+
+
+print(f'The celsius is {celsius} and converted Fahrenheight is {(celsius * 9/5) + 32}')
---
+++ Reminder.py
@@ -0,0 +1,10 @@
+#Find the remainder when dividing two numbers
+
+a = int (input("Enter first number: "))
+b = int (input("Enter second number: "))
+print(f"Remainder: {a % b}")
+
+#Another one
+a = 20
+b = 3
+print(f"Remainder of {a} and {b} is {a % b}")
