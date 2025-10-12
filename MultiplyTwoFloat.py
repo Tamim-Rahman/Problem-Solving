75dfc7076ce9ce75e4abf204ef35a99b428b168c
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-12 16:29:10 +0300

    commit

--- MultiplyTwoFloat.py
+++ MultiplyTwoFloat.py
@@ -1,8 +1,8 @@
-#Multiply two floats
+#Multiply two floats with round
 
 #Solution
 
 number1 = float(input("Enter first number: "))
 number2 = float(input("Enter second number: "))
 
-print(f'Multiplication of two number is: {number1*number2}')
+print(f'Multiplication of two number is: {round(number1*number2, 4)}')
---
+++ PrintType.py
@@ -0,0 +1,11 @@
+#Print the type of 42
+
+#Solution
+
+a = 2
+b = 3.4
+c = "Tamim"
+d = True
+
+
+print(f'The type of a is: {type(a)} \n The type of b is: {type(b)}\n The type of c is: {type(c)}\n The type of d is: {type(d)}')
