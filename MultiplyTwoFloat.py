38a0326e2365374e4ada0f40439b9f2f58b44a0d
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-12 16:05:53 +0300

    MultiplyTwoFloat

---
+++ MultiplyTwoFloat.py
@@ -0,0 +1,8 @@
+#Multiply two floats
+
+#Solution
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+print(f'Multiplication of two number is: {number1*number2}')
