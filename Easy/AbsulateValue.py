443a1484e32b36b40b69bb8ba637d33ad673705b
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-19 16:57:24 +0300

    AbsulateValue

---
+++ Easy/AbsulateValue.py
@@ -0,0 +1,8 @@
+#Problem
+#Print absolute value of -10
+
+
+#Solution
+
+x = -10
+print(abs(x))
---
+++ Easy/RoundFunction.py
@@ -0,0 +1,7 @@
+#Problem
+#Round 3.14159 to 2 decimals
+
+#Solution
+
+x = 3.14159
+print(round(x,2))
