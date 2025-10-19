58dbf8e41780e0a8aa9d284c52c35450e6ff5d78
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-19 17:10:49 +0300

    Reverse

---
+++ Easy/StringToFloat.py
@@ -0,0 +1,9 @@
+#Problem
+#Convert "3.14" to float
+
+#Solution
+
+x = "3.14"
+y = float(x)
+print(y)
+print(type(y))
---
+++ Medium/ReverseString.py
@@ -0,0 +1,7 @@
+#Problem
+#Reverse a string using slicing
+
+#Solution
+
+word = input("Enter any value for for reverse: ")
+print(word[::-1])
