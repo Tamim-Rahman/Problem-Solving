971a8ba7942e6e272cebd33b8825d8b13828bdcd
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-19 16:24:51 +0300

    commit

---
+++ Easy/StringtoInt.py
@@ -0,0 +1,11 @@
+#problem 
+#Convert string "123" to integer and then check the type
+
+#Solution
+
+x = "111"       #String value
+y = int(x)      #Convert string to int
+print(y)        #Print int value
+print(type(y))  #Check the type
+
+
