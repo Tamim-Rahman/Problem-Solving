da1aea83710508b560da492ba5a3d1bf56a4158f
Tamim-Rahman <ttamim.tr@gmail.com>
2025-10-26 15:40:44 +0200

    commit

---
+++ Easy/PositiveNumber.py
@@ -0,0 +1,10 @@
+#problem
+#Check if number > 0 → print "Positive"
+
+#Solution
+
+num = int(input("Enter a number:"))
+if num > 0:
+    print("Positive number")
+else:
+    print("Not a positive number")
---
+++ Medium/ContainsOnlyDigit.py
@@ -0,0 +1,7 @@
+#Problem
+#Check if a string contains only digits, it will return a Bool.
+
+#Solution
+
+word = input("Enter any string: ")
+print(word.isdigit())
